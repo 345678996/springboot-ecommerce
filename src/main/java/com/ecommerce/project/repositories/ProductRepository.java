10db56eacbe173273dff4db4778369ed027f890b
Kanav Rohatgi <kanavr@gmail.com>
2025-07-08 10:44:35 +0530

    add product repo

---
+++ src/main/java/com/ecommerce/project/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.ecommerce.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ecommerce.project.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+    
+
+}
