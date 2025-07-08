0b7acbe9125340c06c3d34abbb0af373368fb0f8
Kanav Rohatgi <kanavr@gmail.com>
2025-07-08 12:34:04 +0530

    add save product fuctionality

--- src/main/java/com/ecommerce/project/controller/ProductController.java
+++ src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -1,8 +1,26 @@
 package com.ecommerce.project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.service.ProductService;
 
 @RestController
 public class ProductController {
-    
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/api/admin/categories/{categoryId}/product")
+    public ResponseEntity<ProductDTO> addProduct(@RequestBody Product product,
+                                                 @PathVariable Long categoryId) {
+        ProductDTO productDTO = productService.addProduct(product, categoryId);
+        return new ResponseEntity<>(productDTO, HttpStatus.CREATED);
+    }
 }
